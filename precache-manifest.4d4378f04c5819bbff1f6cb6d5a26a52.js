cdb8aa67f56b6f6a168326f83209d431d4a687ec
Calamatitum <ponomaryovalena@mail.ru>
2019-07-17 22:54:41 +0500

    Updates

---
+++ precache-manifest.4d4378f04c5819bbff1f6cb6d5a26a52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc6ed37f3d2648eb77c8",
+    "url": "/note-app-React-Redux/static/js/runtime~main.454ad62f.js"
+  },
+  {
+    "revision": "f46f37f69fdb22b98977",
+    "url": "/note-app-React-Redux/static/js/main.52e39980.chunk.js"
+  },
+  {
+    "revision": "cd5f4b6ad46ad321bd82",
+    "url": "/note-app-React-Redux/static/js/2.da598568.chunk.js"
+  },
+  {
+    "revision": "f46f37f69fdb22b98977",
+    "url": "/note-app-React-Redux/static/css/main.ac84e607.chunk.css"
+  },
+  {
+    "revision": "cd5f4b6ad46ad321bd82",
+    "url": "/note-app-React-Redux/static/css/2.71e89413.chunk.css"
+  },
+  {
+    "revision": "7051f6e3adf17bd8a7dc63d8d0bf538f",
+    "url": "/note-app-React-Redux/index.html"
+  }
+];
